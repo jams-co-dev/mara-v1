741258da85468f4667fc33b19dbf28080dd137fa
Johan Col dev <jams.co.dev@gmail.com>
2025-08-19 03:07:54 +0000

    El efecto que tenemos puede ser como un barrido horizontal

--- src/components/page-wrapper.tsx
+++ src/components/page-wrapper.tsx
@@ -7,10 +7,10 @@ import type { ReactNode } from "react";
 export function PageWrapper({ children, className }: { children: ReactNode; className?: string }) {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 40 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: 40 }}
-      transition={{ duration: 0.8 }}
+      initial={{ opacity: 0, x: 300 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: -300 }}
+      transition={{ duration: 0.5 }}
       className={cn(className)}
     >
       {children}
