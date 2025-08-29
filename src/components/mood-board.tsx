ab1f0ab81b005db9205ffed73702ea7bbfcd916b
Johan Col dev <jams.co.dev@gmail.com>
2025-08-29 17:33:23 +0000

    no me esta mostrando el popup, sigue sin funcionar. puedes revisar

--- src/components/mood-board.tsx
+++ src/components/mood-board.tsx
@@ -25,17 +25,27 @@ function getLayoutClasses(itemCount: number) {
     }
 }
 
-function BackgroundVideoItem({ item, onVideoSelect, className }: { item: VideoData; onVideoSelect: (video: VideoData) => void; className?: string; }) {
+interface BackgroundVideoItemProps {
+    item: VideoData;
+    onVideoSelect: (video: VideoData) => void;
+    className?: string;
+}
+
+function BackgroundVideoItem({ item, onVideoSelect, className }: BackgroundVideoItemProps) {
     return (
-        <motion.div
-            onClick={() => onVideoSelect(item)}
+        <div
             className={cn("relative h-full group overflow-hidden cursor-pointer", className)}
         >
+            <div 
+                onClick={() => onVideoSelect(item)}
+                className="absolute inset-0 z-10"
+                aria-label={`Open video ${item.title}`}
+            />
             <iframe
                 src={`https://player.vimeo.com/video/${item.videoId}?background=1&autoplay=1&loop=1&muted=1&title=0&byline=0&portrait=0`}
                 frameBorder="0"
                 allow="autoplay; fullscreen; picture-in-picture"
-                className="absolute top-1/2 left-1/2 w-auto h-auto min-w-[177.77vh] min-h-[100vw] -translate-x-1/2 -translate-y-1/2 transition-transform duration-300 ease-in-out group-hover:scale-105"
+                className="absolute top-1/2 left-1/2 w-auto h-auto min-w-[177.77vh] min-h-[100vw] -translate-x-1/2 -translate-y-1/2 transition-transform duration-300 ease-in-out group-hover:scale-105 pointer-events-none"
                 title={item.title}
             ></iframe>
             <div className="absolute bottom-0 left-0 p-4 bg-gradient-to-t from-black/60 to-transparent w-full pointer-events-none">
@@ -43,14 +53,14 @@ function BackgroundVideoItem({ item, onVideoSelect, className }: { item: VideoDa
                     {item.title}
                 </h3>
             </div>
-        </motion.div>
+        </div>
     );
 }
 
 
-export function MoodBoard({ rows, onVideoSelect, ...props }: MoodBoardProps) {
+export function MoodBoard({ rows, onVideoSelect }: MoodBoardProps) {
   return (
-    <div className="w-full relative" {...props}>
+    <div className="w-full relative">
       <div className="flex flex-col">
         {rows.map((row, rowIndex) => (
           <div key={rowIndex} className="flex flex-col md:flex-row aspect-video md:h-[50vh] w-full">
