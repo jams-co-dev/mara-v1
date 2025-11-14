7c6d813dd2dc9154089b19d78cdeafd193b7e1f7
Johan Col dev <jams.co.dev@gmail.com>
2025-11-14 01:25:44 +0000

    Puedes setear el color de fondo de la pagina About Us de color negro

--- src/app/about/about-client-page.tsx
+++ src/app/about/about-client-page.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 export default function AboutClientPage() {
 
   return (
-    <div className="bg-background text-primary-foreground">
+    <div className="bg-black text-primary-foreground">
       <div className="flex flex-col min-h-[calc(100vh_-_5rem)]">
         <main className="flex-grow pt-16 pb-24">
           <div className="container mx-auto px-4">
@@ -20,7 +20,7 @@ export default function AboutClientPage() {
               <div className="max-w-7xl mx-auto relative flex flex-col md:justify-center md:items-center">
                 <div className="w-full">
                   <Image
-                    src="/images/About_us.png"
+                    src="/images/About_us_2.png"
                     alt="Mara Post Team"
                     width={1280}
                     height={720}
@@ -28,7 +28,7 @@ export default function AboutClientPage() {
                     data-ai-hint="team collaboration"
                   />
                 </div>
-                <div className="relative md:absolute md:left-0 md:top-1/2 w-full max-w-sm p-6 rounded-lg backdrop-blur-sm -ml-0 md:-ml-8 mt-4 md:mt-8">
+                <div className="relative md:absolute md:left-0 md:top-1/2 w-full max-w-sm p-6 rounded-lg backdrop-blur-sm -ml-0 md:-ml-8 mt-4 md:mt-0">
                   <p className="text-xs text-white/90 mb-4">
                     We are transforming alchemists of atmospheres, exploring and creating new ways of telling stories. We see how images find their voice, and how light and shadow dance with colors and textures, finding perfect harmony. We are that magical essence that transforms reality as far as imagination can take you. We are Mara Post.
                   </p>
